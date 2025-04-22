fe0a499c571e60641851edb04ecc01f2d8f886a5
Cpiggott-lab <crpapc@gmail.com>
2025-04-22 12:13:39 +0200

    done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,95 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.querySelector("#game-intro");
+    this.gameScreen = document.querySelector("#game-screen");
+    this.gameEndScreen = document.querySelector("#game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "./images/car.png"
+    );
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.intervalId = null;
+    this.gameLoopFrequency = Math.round(1000 / 60);
+  }
+  start() {
+    this.gameScreen.style.width = `${this.width}px`;
+    this.gameScreen.style.height = `${this.height}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+  gameLoop() {
+    this.update();
+    if (this.gameIsOver) clearInterval(this.gameIntervalId);
+  }
+  update() {
+    this.player.move();
+    console.log("in the update");
+    for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
+      obstacle.move();
+
+      // If the player's car collides with an obstacle
+      if (this.player.didCollide(obstacle)) {
+        // Remove the obstacle element from the DOM
+        obstacle.element.remove();
+        // Remove obstacle object from the array
+        this.obstacles = [];
+        // Reduce player's lives by 1
+        this.lives--;
+        // Update the counter variable to account for the removed obstacle
+        i--;
+        // To actually have the lives count work
+
+        const lives = document.querySelector("#lives");
+        lives.textContent = this.lives;
+        // If the lives are 0, end the game
+        if (this.lives === 0) {
+          this.endGame();
+        }
+      } // If the obstacle is off the screen (at the bottom)
+      else if (obstacle.top > this.height) {
+        // Increase the score by 1
+        this.score++;
+        const score = document.querySelector("#score");
+        score.textContent = this.score;
+        // Remove the obstacle from the DOM
+        obstacle.element.remove();
+        // Remove obstacle object from the array
+        this.obstacles = [];
+        // Update the counter variable to account for the removed obstacle
+        i--;
+      }
+    }
+
+    // Create a new obstacle based on a random probability
+    // when there is no other obstacles on the screen
+    if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+  }
+
+  // Create a new method responsible for ending the game
+  endGame() {
+    this.player.element.remove();
+    this.obstacles.forEach((obstacle) => obstacle.element.remove());
+
+    this.gameIsOver = true;
+
+    // Hide game screen
+    this.gameScreen.style.display = "none";
+    // Show end game screen
+    this.gameEndScreen.style.display = "block";
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,74 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game = null;
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
+    game = new Game();
+    game.start();
     console.log("start game");
+  }
+
+  window.addEventListener("keydown", handleKeydown);
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  window.addEventListener("keyup", handleKeyup);
+  function handleKeyup(event) {
+    const key = event.key;
+    const possibleKeyStrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+    if (possibleKeyStrokes.includes(key)) {
+      event.preventDefault();
+
+      if (key === "ArrowLeft" || key === "ArrowRight") {
+        game.player.directionX = 0;
+      }
+      if (key === "ArrowUp" || key === "ArrowDown") {
+        game.player.directionY = 0;
+      }
+    }
+    // Add an event listener to the restart button
+    restartButton.addEventListener("click", function () {
+      // Call the restartGame function when the button is clicked
+      restartGame();
+    });
+
+    // The function that reloads the page to start a new game
+    function restartGame() {
+      location.reload();
+    }
   }
 };
